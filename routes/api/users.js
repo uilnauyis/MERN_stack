a0cce8bdd2890eba49be4842c2880a41c68173ee
uilnauyis <uilnauyis@gmail.com>
2019-07-01 16:54:44 +1200

    Set up environment

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
-router.get(
+router.post(
   '/',
   [
     check('name', 'Name cannot be left blank')
@@ -14,11 +17,37 @@ router.get(
       'Please set a password with minimum length of 8'
     ).isLength({ min: 8 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({
+        email
+      });
+      if (user) {
+        res
+          .status(400)
+          .json({ errors: [{ message: 'This user already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: 200,
+        r: 'pg',
+        d: 'mm'
+      });
+
+      const newUser = new User({ name, email, avatar, password });
+      const salt = await bcrypt.getSalt(10);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+
     console.log(req.body);
     res.send('User route');
   }
